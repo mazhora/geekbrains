d91260612dfe4114ebee73fa6793172f1efe2746
Evgeny Mazhora <evg@mazhora.ru>
2019-02-21 21:25:04 +0300

    LessonFour

--- LessonFour/LessonFour.c
+++ LessonFour/LessonFour.c
@@ -6,3 +6,31 @@
  */
 
 
+#include <stdio.h>
+
+int main (int argc, const char* argv[]){
+
+	int a, b;
+	a = 10;
+	b = 20;
+	int count = 0;
+	while (a < b) {
+		if ( (a % 2) == 0)
+			count++;
+		printf("%d\n", a);
+		a++;
+	}
+	printf("всего четных %d\n", count);
+
+	int digit;
+	do {
+		printf("введите число на которое нужно разделить 100\n");
+		scanf("%d", &digit);
+	} while ( digit == 0);
+	printf("100 / %d = %d\n", digit,  100 / digit);
+	printf("остаток от деления %d\n", 100 % digit);
+
+
+
+	return 0;
+}
