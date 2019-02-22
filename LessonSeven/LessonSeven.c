120d1b06bab9aa6bdd3552f677d58f87e54726ca
Evgeny Mazhora <evg@mazhora.ru>
2019-02-22 16:41:14 +0300

    LessonSeven

--- LessonSeven/LessonSeven.c
+++ LessonSeven/LessonSeven.c
@@ -5,4 +5,49 @@
  *      Author: user
  */
 
+#include <stdio.h>
 
+
+void swap(int * x, int * y){
+
+	*x = *x ^ *y;
+	*y = *y ^ *x;
+	*x = *x ^ *y;
+}
+
+
+int main (int argc, const char* argv[]){
+
+	/*
+	int c,d;
+	printf("enter digit: \n");
+	scanf("%d", &c);
+	printf("enter next digit: \n");
+	scanf("%d", &d);
+
+*/
+	int c;
+	c = 50;
+
+	printf("value of c is %d \n", c);
+	printf("address of c is \t %p \n", &c);
+
+	int * pointer;
+	pointer = &c;
+
+	printf("value of c pointer is \t %p \n", pointer);
+	printf("value of pointer address is %d \n", *pointer);
+
+
+	int a,b;
+	a = 50;
+	b = 100;
+
+	printf("a = %d and b = %d\n", a,b);
+
+	swap(&a,&b);
+
+	printf("a = %d and b = %d\n", a,b);
+
+	return 0;
+}
