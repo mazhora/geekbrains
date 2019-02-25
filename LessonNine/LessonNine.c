0929737c4fa64058e6e720321d713f043b4fac08
Evgeny Mazhora <evg@mazhora.ru>
2019-02-25 14:19:39 +0300

    Lesson9

--- LessonNine/LessonNine.c
+++ LessonNine/LessonNine.c
@@ -6,3 +6,47 @@
  */
 
 
+#include <stdio.h>
+#define SIZE 10
+
+void printArray(int *array, int lenght){
+	int i;
+	for (i=0; i < lenght; i++){
+		printf("%d = ", i);
+		printf("%d\n", *(array + i));
+	}
+}
+
+float averge(int *array, int lenght){
+	float result = 0;
+	int i;
+	for (i=0; i < lenght; i++){
+		result += *(array + i);
+	}
+	return result / lenght;
+}
+
+int main (int argc, const char* argv[]){
+
+	int array[SIZE];
+
+	int i = 0;
+	float result = 0;
+
+	while (i<SIZE){
+		printf("введите число:\n");
+		scanf("%d", array + i);
+		i++;
+	}
+	printf("\nсреднее арифметическое: \n");
+	for (i=0; i < SIZE; i++){
+		printf("%d = ", i);
+		printf("%d\n", array[i]);
+		result += *(array + i);
+	}
+	printf("\n%f\n", result / SIZE);
+	printf("\n============\n");
+	printArray(array, SIZE);
+	printf("\n%f\n", averge(array, SIZE));
+	return 0;
+}
