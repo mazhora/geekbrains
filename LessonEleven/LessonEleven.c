b6eee5a2fefef66af5ce48cf295d85e50eef6b4d
Mazhora Evgeny <evg@mazhora.ru>
2019-02-26 01:01:55 +0300

    L11

--- LessonEleven/LessonEleven.c
+++ LessonEleven/LessonEleven.c
@@ -6,3 +6,27 @@
  */
 
 
+#include <stdio.h>
+
+char* lalala(){
+	return "Hello";
+}
+
+int main (int argc, const char* argv[]){
+
+	char charr[256] = "это строка!";
+	char* chpoint = "это тоже строка";
+
+	printf("%s\n", charr);
+	printf("%s\n", chpoint);
+
+
+	charr[5] = 'X';
+	printf("%s\n", charr);
+
+
+	printf("%s\n", lalala());
+        
+	return 0;
+}
+
