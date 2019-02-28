a0422e6702d2477a190b50bdae12d041a56fd5c2
Evgeny Mazhora <evg@mazhora.ru>
2019-02-28 19:49:34 +0300

    lesson 14

--- LessonFourteen/LessonFourteen.c
+++ LessonFourteen/LessonFourteen.c
@@ -5,4 +5,36 @@
  *      Author: user
  */
 
+#include <stdio.h>
+#define SIZE 10
 
+int main(int argc, const char* argv[]){
+
+	int* a = (int*)malloc(sizeof(int)*SIZE);
+//	int* a = (int*)calloc(SIZE, sizeof(int));
+	int array[SIZE];
+	int i;
+	for (i = 0; i < SIZE; i++) array[i] = i *10;
+	for (i = 0; i < SIZE; i++) printf("%d ", array[i]);
+	puts("");
+
+	for (i = 0; i < SIZE; i++) *(a + i) = i *10;
+	for (i = 0; i < SIZE; i++) printf("%d ", *(a + i));
+	printf("\n");
+//	free(a);
+	int newsize = SIZE + 10;
+	a = realloc(a, sizeof(int)*newsize);
+	for (i = 0; i < newsize; i++) *(a + i) = i *10;
+	for (i = 0; i < newsize; i++) printf("%d ", *(a + i));
+	printf("\n");
+	for (i = 0; i < SIZE; i++) printf("%d ", array[i]);
+	printf("\n");
+	free(a);
+	for (i = 0; i < newsize; i++) printf("%d ", *(a + i));
+	printf("\n");
+	for (i = 0; i < SIZE; i++) printf("%d ", array[i]);
+	printf("\n");
+
+
+	return 0;
+}
