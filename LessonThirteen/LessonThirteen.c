79fb0ef5e4cd8f14ee7f8daaac9e7f3b9a498b4b
Evgeny Mazhora <evg@mazhora.ru>
2019-02-28 19:04:45 +0300

    lesson 13

--- LessonThirteen/LessonThirteen.c
+++ LessonThirteen/LessonThirteen.c
@@ -5,4 +5,24 @@
  *      Author: user
  */
 
+#include <stdio.h>
 
+int main(int argc, const char* argv[]){
+
+	FILE *f;
+	f = fopen("test.txt", "w");
+	if (f == NULL)
+		return 1;
+	fprintf(f, "hello %s \n0", "lalala\n");
+	fclose(f);
+
+	f = fopen("test.txt", "r");
+	char into[256];
+	while (!feof(f)){
+		fscanf(f, "%s", &into);
+		printf("%s ", into);
+	}
+	fclose(f);
+
+	return 0;
+}
